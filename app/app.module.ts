7c0da4bb475ec082aa59b82a3c70b42b1ab67811
Pankaj Parkar <parkarpankaj3@gmail.com>
2017-05-17 02:20:57 +0530

    Added bootstrap and made UI stuffed with bootstrap.

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TestComponent } from './test/test.component';
 
 @NgModule({
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, NgbModule.forRoot()],
     declarations: [AppComponent, DemoComponent, TestComponent],
     bootstrap: [AppComponent]
 })
