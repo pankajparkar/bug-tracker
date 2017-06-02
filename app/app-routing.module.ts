bd2d68ede6591b15a8fc17eeec1bb08acc63ac63
Pankaj Parkar <parkarpankaj3@gmail.com>
2017-06-02 14:38:04 +0530

    Made routing back to html5 mode and added title for the product page.

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -31,9 +31,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, 
-      {useHash: true}
-    )
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
 })
