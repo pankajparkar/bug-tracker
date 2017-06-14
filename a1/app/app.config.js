e79dedef5e0274c728cd8f0f2c8a3a886b0888d5
Pankaj Parkar <parkarpankaj3@gmail.com>
2017-06-14 10:48:52 +0530

    Made component working with ui-router
    Made all routerLink to ui-sref.

--- a1/app/app.config.js
+++ a1/app/app.config.js
@@ -3,7 +3,19 @@
 
     function getStates(){
         return [
-            {name: 'welcome', url: '/welcome', template: '<welcome></welcome>'},
+            {name: 'welcome', url: '/welcome', component: 'welcome'},
+            // { 
+            //     path: 'dashboard', component: DashboardComponent, 
+            //     children : [
+            //     { path: 'ticket-list', component: TicketListComponent },
+            //     { path: 'ticket-kanban', component: TicketKanbanComponent },
+            //     { path: 'ticket-details/:ticketId', component: TicketDetailsComponent },
+            //     { path: '', redirectTo: 'ticket-list', pathMatch: "full" }
+            //     ]
+            // },
+            // { path: 'profile', component: ProfileComponent },
+            // { path: '', redirectTo: "welcome", pathMatch: 'full' },
+            // { path: '**', redirectTo: 'welcome', pathMatch: "full" }
         ]
     }
     function ConfigBlock($stateProvider, $urlRouterProvider){
