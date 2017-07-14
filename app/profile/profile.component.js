73e145c800c823292eeadc76eb837e6f3061a4c6
Pankaj Parkar <parkarpankaj3@gmail.com>
2017-07-14 19:16:02 +0530

    Added new Profile edit form

--- app/profile/profile.component.js
+++ app/profile/profile.component.js
@@ -10,16 +10,32 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var localstorage_service_1 = require("../shared/localstorage.service");
 var ProfileComponent = (function () {
-    function ProfileComponent() {
+    function ProfileComponent(localStorageService) {
+        this.localStorageService = localStorageService;
     }
-    ProfileComponent.prototype.ngOnInit = function () { };
+    ProfileComponent.prototype.ngOnInit = function () {
+        this.user = this.localStorageService.getObject('user');
+        if (!this.user.Edited) {
+            this.user.FirstName = 'Wayne';
+            this.user.LastName = 'Rooney';
+            this.user.DateOfBirth = new Date(1985, 10, 24);
+            this.user.Address = {
+                StreetAddress: 'ABC street',
+                City: 'Manchestor',
+                State: 'UK',
+                Pin: 123222
+            };
+        }
+    };
     ProfileComponent = __decorate([
         core_1.Component({
             selector: 'profile',
             templateUrl: '/app/profile/profile.component.html',
+            styleUrls: ['/app/profile/profile.component.css']
         }),
-        __metadata("design:paramtypes", [])
+        __metadata("design:paramtypes", [localstorage_service_1.LocalStorageService])
     ], ProfileComponent);
     return ProfileComponent;
 }());
--- app/profile/profile.component.ts
+++ app/profile/profile.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LocalStorageService } from '../shared/localstorage.service';
+
 @Component({
     selector: 'profile',
     templateUrl: '/app/profile/profile.component.html',
-    
+    styleUrls: ['/app/profile/profile.component.css']
 })
 
 export class ProfileComponent implements OnInit {
-    constructor() { }
+    user: any;
+    constructor(private localStorageService: LocalStorageService) { }
 
-    ngOnInit() { }
+    ngOnInit() { 
+        this.user = this.localStorageService.getObject('user');
+        if(!this.user.Edited){
+            this.user.FirstName = 'Wayne';
+            this.user.LastName = 'Rooney';
+            this.user.DateOfBirth = new Date(1985, 10, 24);
+            this.user.Address = {
+                StreetAddress: 'ABC street',
+                City: 'Manchestor',
+                State: 'UK',
+                Pin: 123222
+            };
+        }
+    }
 }
